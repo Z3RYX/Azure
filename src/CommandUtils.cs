53f2ef123e0433797cfbcf5f94bd14f8ac808efe
Z3RYX <zeryxofficial@gmail.com>
2019-06-25 01:45:03 +0200

    Added Command Handler and CommandUtils
    Thought I added command handler in last commit lol
    CommandUtils has method HasPermissions to make that a bit easier for me

---
+++ src/CommandUtils.cs
@@ -0,0 +1,24 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure
+{
+    public static class CommandUtils
+    {
+        public static bool HasPermissions(SocketGuildUser user, GuildPermission[] permissions)
+        {
+            foreach (var permission in permissions) {
+                var userPerms = user.GuildPermissions.ToList();
+
+                if (!userPerms.Contains(permission)) return false;
+            }
+
+            return true;
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,13 @@
 ﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,12 +20,18 @@ namespace Azure
             => new Program().MainAsync().GetAwaiter().GetResult();
 
         private DiscordSocketClient _client;
-
+        private CommandService _commands;
+        private IServiceProvider _services;
         private Config BotConfig;
 
         public async Task MainAsync()
         {
             _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
 
             if (File.Exists("../../config.json")) {
                 BotConfig = JsonConvert.DeserializeObject<Config>(File.ReadAllText("../../config.json"));
@@ -33,9 +42,9 @@ namespace Azure
                 Environment.Exit(0);
             }
 
-            Console.WriteLine(BotConfig.InviteLink);
-
             _client.Log += Log;
+
+            await RegisterCommandsAsync();
 
             await _client.LoginAsync(TokenType.Bot, BotConfig.Token);
             await _client.StartAsync();
@@ -47,6 +56,37 @@ namespace Azure
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
+        }
+
+        private async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(
+                assembly: Assembly.GetEntryAssembly(),
+                services: null);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            if (!(messageParam is SocketUserMessage message)) return;
+
+            int argpos = 0;
+
+            if (!(message.HasStringPrefix(BotConfig.Prefix, ref argpos) ||
+                message.HasMentionPrefix(_client.CurrentUser, ref argpos)) ||
+                message.Author.IsBot)
+                return;
+
+            var ctx = new SocketCommandContext(_client, message);
+
+            var result = await _commands.ExecuteAsync(
+                context: ctx,
+                argPos: argpos,
+                services: null);
+
+            if (!result.IsSuccess)
+                Console.WriteLine(result.ErrorReason);
         }
     }
 }
