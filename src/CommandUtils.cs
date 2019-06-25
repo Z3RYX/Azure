32fb3757b29d963b33a98a779ed0e6de8df563ae
Z3RYX <zeryxofficial@gmail.com>
2019-06-25 03:15:32 +0200

    Added Overloads

--- src/CommandUtils.cs
+++ src/CommandUtils.cs
@@ -20,5 +20,29 @@ namespace Azure
 
             return true;
         }
+
+        public static bool HasPermission(SocketGuildUser user, GuildPermission permission)
+        {
+            var userPerms = user.GuildPermissions.ToList();
+
+            if (userPerms.Contains(permission)) return true;
+
+            return false;
+        }
+
+        public static bool HasPermissions(SocketGuildUser user, GuildPermission[] permissions, out List<GuildPermission> missingPerms)
+        {
+            missingPerms = new List<GuildPermission>();
+
+            foreach (var permission in permissions) {
+                var userPerms = user.GuildPermissions.ToList();
+
+                if (!userPerms.Contains(permission)) missingPerms.Add(permission);
+            }
+
+            if (missingPerms.Count > 0) return false;
+
+            return true;
+        }
     }
 }
